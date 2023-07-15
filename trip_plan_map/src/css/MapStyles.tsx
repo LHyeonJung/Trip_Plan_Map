f7785402921dfd87a21483feacd0ed1d532354ea
hyeonjung <hyeonjung@pentasecurity.com>
2023-07-16 05:08:55 +0900

    1. 검색 결과 팝업 외 영역 클릭 시, 팝업 닫기 / 2. 팝업 위치 가운데로 변경

--- trip_plan_map/src/css/MapStyles.tsx
+++ trip_plan_map/src/css/MapStyles.tsx
@@ -5,6 +5,7 @@ export const searchResultPopup: CSSProperties = {
   position: "absolute",
   zIndex: "10",
   top: 0,
-  left: 0,
+  left: "33%",
   marginTop: "63px",
+  boxShadow: "0 5px 10px rgba(0,0,0,0.3)",
 };
